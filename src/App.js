51a2043f9588b341640eb83162b4f6654913e3a3
deepakbharadwaj6726 <139285750+deepakbharadwaj6726@users.noreply.github.com>
2023-08-03 22:07:59 -0700

    updated navbar

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path={'/home'} element={< Home/>} />
+          <Route path={'/' }  element={< Home/>} />
           <Route path={'/about'} element={<About />} />
           <Route path={'/contact'} element={<Contact />} />
           <Route path={'/services'} element={<Services />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ function Header() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand as={Link} to={'/home'}>FLAMEBUDZ</Navbar.Brand>
+          <Navbar.Brand as={Link} to={'/'}>FLAMEBUDZ</Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to={'/home'}>Home</Nav.Link>
+            <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
             <Nav.Link as={Link} to={'/about'} >About</Nav.Link>
             <Nav.Link as={Link} to={'/services'}>Services</Nav.Link>
             <Nav.Link as={Link} to={'/contact'}>Contact</Nav.Link>
